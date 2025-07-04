edd8ab98187de372b07704d0325d05c6805e75e7
Ernstudy <hey@ernstudy.com>
2025-07-04 11:03:58 -0600

    potography seb site url

---
+++ app/photography-website/page.jsx
@@ -0,0 +1,11 @@
+export default function PhotographyRedirect() {
+  return (
+    <iframe
+      src="https://photography-web-site.vercel.app"
+      width="100%"
+      height="100%"
+      style={{ border: "none", minHeight: "100vh" }}
+      title="Photography Website"
+    />
+  );
+}
