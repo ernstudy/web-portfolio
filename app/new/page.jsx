240db7dc34c2285b304997218f2634680e9fc9cd
Ernstudy <hey@ernstudy.com>
2025-07-04 11:12:18 -0600

    redirect and rewrite

---
+++ app/new/page.jsx
@@ -0,0 +1,11 @@
+export default function NewRedirect() {
+  return (
+    <iframe
+      src="https://new-next-app-theta.vercel.app"
+      width="100%"
+      height="100%"
+      style={{ border: "none", minHeight: "100vh" }}
+      title="New Website"
+    />
+  );
+}
