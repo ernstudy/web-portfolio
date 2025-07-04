8ecd9ab65497c54c897c4e471148202e4ce64a94
Ernstudy <hey@ernstudy.com>
2025-07-04 11:48:24 -0600

    add ernews an d weather-app

---
+++ app/portfolio/ernews/page.jsx
@@ -0,0 +1,19 @@
+export default function PhotographyRedirect() {
+  return (
+    <iframe
+      src="https://ernstudy.github.io/ernews"
+      width="100%"
+      height="100%"
+      style={{
+        border: "none",
+        minHeight: "100vh",
+        position: "fixed",
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+      }}
+      title="Ernews | News App"
+    />
+  );
+}
---
+++ app/portfolio/weather-app/page.jsx
@@ -0,0 +1,11 @@
+export default function WeatherAppRedirect() {
+  return (
+    <iframe
+      src="https://ernstudy.github.io/weather-app"
+      width="100%"
+      height="100%"
+      style={{ border: "none", minHeight: "100vh" }}
+      title="Weather App"
+    />
+  );
+}
