8141c70296242bc881bace7a967eccf1069cca83
Ernstudy <hey@ernstudy.com>
2025-07-04 11:31:47 -0600

    fix project on full screen

--- app/portfolio/photography-website/page.jsx
+++ app/portfolio/photography-website/page.jsx
@@ -4,7 +4,15 @@ export default function PhotographyRedirect() {
       src="https://photography-web-site.vercel.app"
       width="100%"
       height="100%"
-      style={{ border: "none", minHeight: "100vh" }}
+      style={{
+        border: "none",
+        minHeight: "100vh",
+        position: "fixed",
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+      }}
       title="Photography Website"
     />
   );
