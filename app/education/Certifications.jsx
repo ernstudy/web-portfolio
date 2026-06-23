0fa2aa6df364c30de0bf30ebe3c8720613795ef5
ernstudy <saingillese@gmail.com>
2026-06-23 15:22:15 -0400

    feat: update Certifications component to use Next.js Image component and modify subtitle to platform name

--- app/education/Certifications.jsx
+++ app/education/Certifications.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import styles from "./Certifications.module.css";
 import certificationsData from "@/lib/certifications";
 
@@ -14,18 +15,20 @@ export const Certifications = () => {
 
           {/* Card */}
           <div className={styles.card}>
-            <img
+            <Image
               src={cert.image}
               alt={cert.subtitle + " logo"}
               className={styles.certLogo}
               onError={(e) => {
                 e.target.style.display = "none";
               }}
+              width="100"
+              height={100}
             />
             <div className={styles.cardBody}>
               <time className={styles.date}>{cert.date}</time>
               <h3 className={styles.title}>{cert.title}</h3>
-              <span className={styles.subtitle}>{cert.subtitle}</span>
+              <span className={styles.subtitle}>{cert.platform}</span>
               <p className={styles.description}>{cert.description}</p>
             </div>
           </div>
